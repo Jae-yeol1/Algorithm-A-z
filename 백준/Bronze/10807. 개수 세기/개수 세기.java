89a3f6096cc4ae132c4cb76c3180a2aaf4f9505e
PPoTTO_Meow <lmjayoul@gmail.com>
2025-12-13 22:58:19 +0900

    [Bronze V] Title: 개수 세기, Time: 112 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,35 +1,37 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // 첫번째줄 정수
-        int N = Integer.parseInt(br.readLine());
-        
-        // 둘째 줄 문제조건 "둘째 줄에는 정수가 공백으로 구분되어져있다"
+
+
+
+        int x = Integer.parseInt(br.readLine());
+
+        int[] arr1  = new int[x];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        // 입력받은 크기만큼 크기할당
-        int[] num1 = new int[N]; 
-        for(int i = 0 ; i < N ; i++ ){
-            num1[i] = Integer.parseInt(st.nextToken());
+        // 배열에 값
+        for (int i = 0 ; i < x ; i++){
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
-        
-        // 3. readLine은 메소드
-        int v = Integer.parseInt(br.readLine()); 
+
+        // 찾을거
+        int y = Integer.parseInt(br.readLine());
 
         int count = 0;
-        for(int i = 0 ; i < N; i++){
-            if(num1[i] == v){
+        for (int i = 0 ; i < x ; i++){
+            if (arr1[i] == y){
                 count++;
             }
         }
-
         System.out.println(count);
+
+
+
+
     }
 }
