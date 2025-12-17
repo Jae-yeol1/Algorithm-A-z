fd53c124deb604999e82c1852133873da86acc35
PPoTTO_Meow <lmjayoul@gmail.com>
2025-12-17 22:40:22 +0900

    [Bronze V] Title: 문자와 문자열, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,19 +1,18 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException{
-
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-   
-        String str = br.readLine();
-        int istr = Integer.parseInt(br.readLine())-1;
+        String input = br.readLine();
+        int num = Integer.parseInt(br.readLine());
 
-        
-        System.out.println(str.charAt(istr));
+        System.out.println(input.charAt(num-1));
+
+
+
     }
 }
