ce418a46698680f877d940b683cf1b00c284d4e7
PPoTTO_Meow <lmjayoul@gmail.com>
2025-12-27 00:25:00 +0900

    [Bronze IV] Title: 특별한 날, Time: 96 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int month = Integer.parseInt(br.readLine());
+        int day = Integer.parseInt(br.readLine());
+
+        if (month < 2 || (month == 2 && day < 18)) {
+            // 2월 18
+            System.out.println("Before");
+        } else if (month > 2 || (month == 2 && day > 18)) {
+            System.out.println("After");
+        } else {
+            System.out.println("Special");
+        }
+    }
+}
