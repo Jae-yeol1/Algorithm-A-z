f5feb192448ad679669d6488706ea68c42e5b014
Jae-yeol1 <lmjayoul@gmail.com>
2026-01-01 23:48:16 +0900

    stack 문제 틀잡기

---
+++ 백준/푸는중/stack/src/main/java/stack_12789/Main.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 학생 수
+        int N = Integer.parseInt(br.readLine());
+
+        // 줄서있는 학생들 번호
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // Stack 자료구조 사용
+        Stack<Integer> stack = new Stack<>();
+
+        // 현재 간식 배급순서
+        int current = 1;
+
+        while (st.hasMoreTokens()) {
+            int student = Integer.parseInt(st.nextToken());
+
+            // 현재순서와 상태가 맞으면 상태 ++
+            if (student == current) {
+                current++;
+            }
+            // 아니면 스택으로 보내는데 이후 ...?
+            else {
+                stack.push(student);
+            }
+
+            // pop 구현 ...?
+
+        }
+
+
+        if (stack.isEmpty()) {
+            System.out.println("Nice");
+        } else {
+            System.out.println("Sad");
+        }
+    }
+}
