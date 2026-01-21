9a1c95baa13223280f2adca5c9885fae3eb4feba
PPoTTO_Meow <lmjayoul@gmail.com>
2026-01-21 23:06:43 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 116 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String crim = "";
+
+        for (int i = 0; i < N; i++) {
+            String name = br.readLine();
+            if (name.contains("S")) {   
+                crim = name;                  
+            }
+        }
+
+        System.out.print(crim);
+    }
+}
