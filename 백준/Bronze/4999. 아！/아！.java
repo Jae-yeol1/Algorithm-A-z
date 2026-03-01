1016ff9fb066b9953b5f0b3b86dd80fc515e354b
PPoTTO_Meow <lmjayoul@gmail.com>
2026-03-01 23:57:55 +0900

    [Bronze V] Title: 아!, Time: 164 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String jaehwan = sc.next();
+        String doctor = sc.next();
+        
+        if (jaehwan.length() >= doctor.length()) {
+            System.out.println("go");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
